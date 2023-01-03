d9a15c8ca2173d9990e505b6c86a62c63bf5bdd3
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-03 06:47:04 +0200

    setup webpack

--- dist/runtime.js
+++ dist/runtime.js
@@ -182,7 +182,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkwebpack_setup"] = self["webpackChunkwebpack_setup"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunktv_hub"] = self["webpackChunktv_hub"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
