1b52ba836dce72abfc54dfbb147e86dc734feb48
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-03 11:02:42 +0200

    Add search functionality to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import _ from 'lodash';
-import './style.css';
-function component() {
-  const element = document.createElement('div');
-
- // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
+function searchShow(query) {
+  const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+  fetch(url)
+  .then(response => response.json())
+  .then((jsonData) => {
+    console.log(jsonData)
+    const results = jsonData.map(element => element.show.name);
+    renderResults(results);
+    document.getElementById("errorMessage").innerHTML = "";
+  })
+  .catch((error) => {
+    document.getElementById("errorMessage").innerHTML = error;
+    renderResults([]);
+  });
 }
 
-document.body.appendChild(component());
+function renderResults(results) {
+  const list = document.getElementById("resultsList");
+  list.innerHTML = "";
+  results.map(result => {
+    const element = document.createElement("li");
+    element.innerText = result;
+    list.appendChild(element);
+  });
+}
+let searchTimeoutToken = 0;
+window.onload = () => {
+  const searchFieldElement = document.getElementById("searchField");
+  searchFieldElement.onkeyup = (event) => {
+    clearTimeout(searchTimeoutToken);
+    if(searchFieldElement.value.trim().length === 0) {
+      return;
+    }
+    searchTimeoutToken = setTimeout(() => {
+      searchShow(searchFieldElement.value);
+    }, 250);
+    
+  }
+}
