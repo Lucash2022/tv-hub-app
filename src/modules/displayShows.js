e9fa71de37e312c111f40dfa63aa736a63a1eda6
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-05 06:57:26 +0200

    Add videos plus likes

--- src/modules/displayShows.js
+++ src/modules/displayShows.js
@@ -5,6 +5,7 @@ class Shows {
     this.API_URL = 'https://api.tvmaze.com/';
     this.like_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/CvrqAzoVr9PCSxK9Vq6U/likes';
     this.shows = [];
+    this.likes = [];
   }
 
   getShows = async (showName) => {
@@ -13,10 +14,19 @@ class Shows {
     this.shows = [...list];
   }
 
+  getLikes = async () => {
+    this.likes = await fetch(this.like_URL).then((response) => response.json());
+  }
+
   displayShows = async () => {
     await this.getShows('the');
+    await this.getLikes();
+
     const showsList = this.shows.reduce((prev, curr) => {
       if (curr.show.image) {
+        const index = this.likes.findIndex((like) => like.item_id === curr.show.id);
+        const msgLikes = index <= 0 ? 0 : this.likes[index].likes;
+
         prev += `
                  <div class="shows">
                  
@@ -26,12 +36,13 @@ class Shows {
                    <div class="show-name">
                      <span>${curr.show.name}</span>
                      <div class="like"> 
-                     <i class="fa fa-heart fa-lg" data-pos=${curr.show.id} id="${curr.show.id}"></i> 
+                     <i class="fa fa-heart fa-lg" data-pos=${curr.show.id} id="${curr.show.id}"></i>
+                     <span>${msgLikes}</span> Likes 
                     </div>
-                    <p >Likes: <span id= p${curr.show.id}></span></p>
+                    
                    </div>
                    <div class="comments">
-                     <button data-id="${curr.show.id}"  class="btn" id=${curr.show.id}>Comments</button>
+                     <button data-id="${curr.show.id}"  class="btn" id="${curr.show.id}">Comments</button>
                      <button class="btn-reservations" id="${curr.show.id}">Reservations</button>
                    </div>
              
@@ -45,7 +56,7 @@ class Shows {
     this.registerLikes();
   }
 
-  addLike = async (itemId) => {
+  addLike = async (itemId, likeButton) => {
     await fetch(this.like_URL, {
       method: 'POST',
       headers: {
@@ -53,16 +64,20 @@ class Shows {
       },
       body: JSON.stringify({ item_id: itemId }),
     }).then((response) => response.text(response)).then((json) => json);
+    await this.getLikes();
+
+    const index = this.likes.findIndex((like) => like.item_id === itemId);
+    const msgLikes = index >= 0 ? this.likes[index].likes : 0;
+    likeButton.nextElementSibling.innerHTML = msgLikes;
   }
 
   registerLikes = () => {
     const likeButtons = document.querySelectorAll('.fa-heart');
     likeButtons.forEach((btn) => btn.addEventListener('click', (e) => {
       e.preventDefault();
-      const movieID = btn.getAttribute('id');
-      const pID = 'p'+movieID;
-      document.getElementById(pID).innerHTML = 1;
-      this.addLike(movieID);
+      const movieID = parseInt(btn.getAttribute('id'), 10);
+      // document.getElementById(movieID).innerHTML = 1;
+      this.addLike(movieID, btn);
     }));
   };
 }
