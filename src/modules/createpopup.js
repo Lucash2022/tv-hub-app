9386aed7aa3112b621a1878624a3cfc59c8cd9b7
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-08 10:38:17 +0200

    Fix elint error

--- src/modules/createpopup.js
+++ src/modules/createpopup.js
@@ -40,12 +40,12 @@ export const createPopUpDetails = (details) => {
           <a id="${details.id === 171 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=cjJLEYMzpjc" target="_blank">HOW I MET YOUR MOTHER Youtube link Youtube link</a> 
           <a id="${details.id === 171 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=cjJLEYMzpjc" target="_blank">HOW I MET YOUR MOTHER Youtube link Youtube link</a> 
           <a id="${details.id === 4 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=2yrviapP5uY" target="_blank">ARROW Youtube link Youtube link</a>
-          <a id="${details.id ===  81 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=KPLWWIOCOOQ" target="_blank">GAME OF THRONES Youtube link Youtube link</a>
-          <a id="${details.id ===  44751 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=SJPJPUpNvDw" target="_blank">SUPERMAN AND LOIS Youtube link Youtube link</a>
-          <a id="${details.id ===  435 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=70Y32si4yb8" target="_blank">SMALLVILLE Youtube link Youtube link</a>
-          <a id="${details.id ===  33352 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=x8UAUAuKNcU" target="_blank">The Lord of the Rings: The Rings of Power Youtube link Youtube link</a>
-          <a id="${details.id ===  28152 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=PJpsRF-TJAE" target="_blank">9-1-1 Youtube link Youtube link</a>
-          <a id="${details.id ===  21845 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=fYlZDTru55g" target="_blank">THE GOOD DOCTOR Youtube link Youtube link</a>
+          <a id="${details.id === 81 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=KPLWWIOCOOQ" target="_blank">GAME OF THRONES Youtube link Youtube link</a>
+          <a id="${details.id === 44751 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=SJPJPUpNvDw" target="_blank">SUPERMAN AND LOIS Youtube link Youtube link</a>
+          <a id="${details.id === 435 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=70Y32si4yb8" target="_blank">SMALLVILLE Youtube link Youtube link</a>
+          <a id="${details.id === 33352 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=x8UAUAuKNcU" target="_blank">The Lord of the Rings: The Rings of Power Youtube link Youtube link</a>
+          <a id="${details.id === 28152 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=PJpsRF-TJAE" target="_blank">9-1-1 Youtube link Youtube link</a>
+          <a id="${details.id === 21845 ? 'showframe' : 'hideframe'}" href="https://www.youtube.com/watch?v=fYlZDTru55g" target="_blank">THE GOOD DOCTOR Youtube link Youtube link</a>
             
           </div>
         <div><span class="titles">ID: </span> ${details.id}</div>
